ea2aceccdb40fc477e21b3067cfd5c7dcfd6425d
Mahmoud <mh8011651@gmail.com>
2025-04-26 14:37:17 +0200

    Add new files and updates

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div className="main">
+      <h2 style={{textTransform: "uppercase"}}>
+        <button>add</button>
+      </h2>
+    </div>
+  );
+}
