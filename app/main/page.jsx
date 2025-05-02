d9fa1a4dc909de324ec2cc1a11e1f847bbb4fc5d
Mahmoud <mh8011651@gmail.com>
2025-05-02 10:14:11 +0200

    Add new files and updates

--- app/main/page.jsx
+++ app/main/page.jsx
@@ -8,11 +8,23 @@ import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from 'swiper/m
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { IoIosArrowDown } from "react-icons/io";
+import { MdDelete } from "react-icons/md";
+import Nav from "../components/Nav/page";
+import Add from "../components/Add/page";
+import { db } from "../firebase";
+import { collection, deleteDoc, onSnapshot, query, where, doc } from "firebase/firestore";
 
 function Main() {
     const [name, setName] = useState('')
     const [userEmail, setUserEmail] = useState('')
     const [coin, setCoin] = useState(0)
+    const [active, setActive] = useState(null)
+    const [add, setAdd] = useState(false)
+    const [habits, setHabits] = useState([])
+    const colors = ['#b8eb6c','#1f3da0', '#f7cd63', '#1b1b1b']
+    const test = ['1', '2', '3', '5','6','78']
+
     const today = new Date()
     const currentYear = today.getFullYear()
     const currentMonth = today.getMonth()
@@ -38,7 +50,20 @@ function Main() {
                 setUserEmail(email)
             }
         }
-    },[])
+        const q = query(collection(db, 'habits'), where('email', '==', userEmail))
+        const unSubscribe = onSnapshot(q, (querySnapShot) => {
+            const habitsArrya = []
+            querySnapShot.forEach((doc) => {
+                habitsArrya.push({...doc.data(), id: doc.id})
+            })
+            setHabits(habitsArrya)
+        })
+        return () => unSubscribe()
+    },[userEmail])
+
+    const hanleDelete = async(id) => {
+        await deleteDoc(doc(db, 'habits', id))
+    }
 
     return(
         <div className="main">
@@ -80,6 +105,37 @@ function Main() {
                         })}
                 </Swiper>
             </div>
+            <div className={styles.habitContainer}>
+                <div className={styles.title}>
+                    <h2>my habits</h2>
+                </div>
+                {habits.map((habit, index) => {
+                    return(
+                    <div key={habit.id} style={{backgroundColor: colors[index % colors.length]}} className={active === index ? `${styles.card} ${styles.open}` : `${styles.card}`} onClick={() => setActive(active === index ? null : index)}>
+                        <div className={styles.cardHeader}>
+                            <div className={styles.cardTitle}>
+                                <h2>
+                                    <span onClick={() => hanleDelete(habit.id)}><MdDelete/></span>
+                                    <span>{habit.name}</span>
+                                </h2>
+                                <strong>{habit.type}</strong>
+                            </div>
+                            <h2><IoIosArrowDown/></h2>
+                        </div>
+                        <div className={styles.cardBody}>
+                            {days.map((day, index) => {
+                                return(
+                                    <p key={index}>{day.dayNumber}</p>
+                                )
+                            })}
+                        </div>
+                    </div>
+                    )
+                })}
+                
+            </div>
+            <Add add={add} setAdd={setAdd}/>
+            <Nav add={add} setAdd={setAdd}/>
         </div>
     )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,22 @@ import styles from "./styles.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../public/images/IMG_7211.PNG"
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
+
+  const handleWhereToGo = () => {
+    if(typeof window !== 'undefined') {
+      const email = localStorage.getItem('email')
+      if(email) {
+        router.push('/main')
+      }else {
+        router.push('/login')
+      }
+    }
+  }
 
   return (
     <div className="main">
@@ -17,7 +30,7 @@ export default function Home() {
           <Image src={logo} fill style={{objectFit: "cover"}} alt="logo image"/>
         </div>
         <div className={styles.bottom}>
-          <Link href={"/login"} className={styles.loadingLink}>Get started</Link>
+          <button onClick={handleWhereToGo} className={styles.loadingLink}>Get started</button>
           <Link href={"/login"} className={styles.accountLink}>I have an account</Link>
         </div>
       </div>
